c18aa1a167bf2e686921fb0cc9c1a5fdeb27a52b
Luke Murray <lukemurraynz@users.noreply.github.com>
2016-12-27 17:01:19 +1300

    Formatting
    Adjusted formatting & added $ENV:HOMEDRIVE instead of hard coding
    C:\temp

--- Clean-AppVClientPackage.ps1
+++ Clean-AppVClientPackage.ps1
@@ -5,44 +5,50 @@
     Version: 0.1
     Version History:
 
-    Purpose: To removed unused App-V packages older than x30 days from a clients App-V package store. Intended to be used with Configuration Manager baslines.
-	  http://luke.geek.nz/win/rm-unused-app-v-packages-powershell-sccm.
+    Purpose: To removed unused App-V packages older than x30 days from a clients App-V package store. Intended to be used as a Configuration Manager baseline.
+    http://luke.geek.nz/win/rm-unused-app-v-packages-powershell-sccm.
 #>
 
+$log = "$env:HOMEDRIVE\temp\$env:COMPUTERNAME".log
 $Now = Get-Date 
 $Days = 30
 $TargetFolder = "$env:ProgramData\App-V" 
 $LastAccessTime = $Now.AddDays(-$Days) 
-$Files = Get-ChildItem -Path $TargetFolder  | Where-Object -FilterScript { 
-  $_.LastAccessTime -lt "$LastAccessTime" 
+$Files = Get-ChildItem -Path $TargetFolder  | Where-Object -FilterScript {
+  $_.LastAccessTime -lt ('{0}' -f $LastAccessTime)
 }  
 $null = @() 
 if ($Files.count -gt 0)
 {
   ForEach ($null in $Files)
   { 
-   Start-Transcript -Append -Path c:\temp\"$env:COMPUTERNAME".log
+    Start-Transcript -Append -Path $log
 
-     $Result = Get-AppvClientPackage | Where-Object { $_.Name -notlike 'Microsoft App-V*'} 
+    $Result = Get-AppvClientPackage | Where-Object -FilterScript {
+      $_.Name -notlike 'Microsoft App-V*'
+    } 
      
     foreach ($PackageId in $Result)
     
     {
-    
-      Get-AppvClientPackage -PackageID $PackageId.PackageID.Guid #| Remove-AppvClientPackage
+      #The line below will remove unused packages. Please remove the '#' before the pipeline to actually remove packages.
+      Get-AppvClientPackage -PackageId $PackageId.PackageID.Guid #| Remove-AppvClientPackage
     }
       
     foreach ($null in $TargetFolder)
     { 
-      Get-ChildItem -Path $TargetFolder | Where-Object {$_.PSIsContainer -eq 'True' -and $_.GetFileSystemInfos().Count -eq 0} |  Remove-Item
+      #The line below will remove  empty folders. Please remove the '#' before the pipeline to actually remove the folders.
+      Get-ChildItem -Path $TargetFolder | Where-Object -FilterScript {
+        $_.PSIsContainer -eq 'True' -and $_.GetFileSystemInfos().Count -eq 0
+      } # |  Remove-Item
     }
     Write-Verbose -Message 'Non-Compliant' -Verbose
-   Stop-Transcript
+    Stop-Transcript
   } 
 }
 else
 {
-  Start-Transcript -Append -Path c:\temp\"$env:COMPUTERNAME".log
+  Start-Transcript -Append -Path $log
   Write-Verbose -Message 'Compliant' -Verbose
   Stop-Transcript
 }
