2c04be5d7e4d0777a887eddaea738284af992ab4
Luke Murray <Luke.Murray@wintec.ac.nz>
2017-03-28 15:30:38 +1300

    Added OEMInfo script.

---
+++ Desktop/Set-OEMInfo.ps1
@@ -0,0 +1,18 @@
+﻿<#
+    Author: Luke Murray (Luke.Geek.NZ)
+    Version: 0.1
+    Purpose: To add in basic OEM Info into the registry. 
+#>
+
+Function Set-OEMInfo
+    {  
+        $OEMKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation'   
+        Set-ItemProperty -Path $OEMKey -Name 'Model' -Value (Get-WmiObject -Class Win32_ComputerSystem).Model   
+        Set-ItemProperty -Path $OEMKey -Name 'HelpCustomized' -Value 00000000   
+        Set-ItemProperty -Path $OEMKey -Name 'SupportHours' -Value '7AM - 5PM'   
+        Set-ItemProperty -Path $OEMKey -Name 'Manufacturer' -Value 'Company Name'   
+        Set-ItemProperty -Path $OEMKey -Name 'SupportPhone' -Value '0118 999 881'  
+        Set-ItemProperty -Path $OEMKey -Name 'SupportURL' -Value 'https://www.luke.geek.nz/'  
+    }
+
+Set-OEMInfo
