761340216ab84cd879f60036d0b0a6f67986ff9a
Luke Murray <lukejmurray@gmail.com>
2017-05-12 14:19:41 +1200

    Update Check-MicTrayExist.ps1

--- Desktop/Check-MicTrayExist.ps1
+++ Desktop/Check-MicTrayExist.ps1
@@ -1,11 +1,25 @@
-if ( (Test-Path -Path $env:windir\System32\MicTray64.exe) -or (Test-Path -Path $env:windir\System32\MicTray.exe) ) { 
+#requires -Version 1.0
+<#
 
-    Remove-Item -Path $env:windir\System32\MicTray64.exe -ErrorAction SilentlyContinue
-    Remove-Item -Path $env:windir\System32\MicTray.exe -ErrorAction SilentlyContinue
-    Remove-Item -Path $env:PUBLIC\MicTray.log -ErrorAction SilentlyContinue
-    Write-Verbose -Message True -Verbose
+    Author: Luke Murray (luke.geek.nz)
+    Version: 0.1
+
+    Purpose: Determines if the HP 'Keylogger' exists and if so deletes the files.
+
+    https://www.bleepingcomputer.com/news/security/keylogger-found-in-audio-driver-of-hp-laptops/
+
+    With the Boolean logic can be easily modified to a Configuration Manager Configuration Baseline.
+
+#>
+
+if ( (Test-Path -Path $env:windir\System32\MicTray64.exe) -or (Test-Path -Path $env:windir\System32\MicTray.exe) ) 
+{
+  Remove-Item -Path $env:windir\System32\MicTray64.exe -ErrorAction SilentlyContinue
+  Remove-Item -Path $env:windir\System32\MicTray.exe -ErrorAction SilentlyContinue
+  Remove-Item -Path $env:PUBLIC\MicTray.log -ErrorAction SilentlyContinue
+  Write-Verbose -Message True -Verbose
 }
 else
- {
+{
   Write-Verbose -Message False -Verbose
- }
+}
