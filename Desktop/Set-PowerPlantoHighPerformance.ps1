c5cbb07fb058bc695ca08f1365ac0c3c45e662a8
Luke Murray <lukejmurray@gmail.com>
2017-06-02 09:40:35 +1200

    Create Set-PowerPlantoHighPerformance.ps1

---
+++ Desktop/Set-PowerPlantoHighPerformance.ps1
@@ -0,0 +1,9 @@
+$cred = Get-Credential
+$computer = 'localhost'
+
+Invoke-Command -ComputerName $computer -Credential $cred -ScriptBlock {
+
+  $p = Get-CimInstance -Name root\cimv2\power -Class win32_PowerPlan -Filter "ElementName = 'High Performance'"          
+
+  Invoke-CimMethod -InputObject $p -MethodName Activate
+}
