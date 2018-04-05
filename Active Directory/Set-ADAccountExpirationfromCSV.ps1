219d80d1149d3bb6eb668a1f06491b63d8b0930c
Luke Murray <lukejmurray@gmail.com>
2018-04-06 11:04:44 +1200

    Import CSV of firstname,lastname and set account expiry

---
+++ Active Directory/Set-ADAccountExpirationfromCSV.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 2.0 -Modules ActiveDirectory
+
+<#
+
+Author: Luke Murray
+Version: 1.0
+Version History:
+
+1.0 - Script creation
+
+Purpose:
+
+To import CSV of users (with Firstname, Lastname values) and change the expiration date for all the accounts in the CSV.
+Remove '-whatif' to actually make change and change the -DateTime value to match how far ahead you want the AD account to be expired.
+
+#>
+
+$users = Import-Csv -Path C:\temp\users.csv           
+           
+foreach ($user in $users) {           
+    $fname = $user.Firstname
+    $lname = $user.Lastname
+ 
+    Get-ADUser -Filter {
+        GivenName -eq $fname  -and Surname -eq $lname
+    } -Properties * |  Set-ADAccountExpiration -DateTime '31/03/2019' -Whatif
+}
