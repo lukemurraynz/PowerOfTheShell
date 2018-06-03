e0618738476216cb691e8c2b59309c07436e0608
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2018-06-04 11:33:23 +1200

    Run-PowerShellAsAdmin initial commit

---
+++ Other/Run-PowerShellAsAdmin.ps1
@@ -0,0 +1,22 @@
+﻿<#
+    Author: Luke Murray (Luke.Geek.NZ)
+    Version: 0.1
+    Version History:
+    Purpose: Elevates Powershell as Admin if not already Admin and runs script.
+#>
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    
+        Exit
+    
+    }
+    
+}
+
+Install-Module AzureRM
