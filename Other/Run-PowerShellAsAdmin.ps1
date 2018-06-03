4fcbb39aad3580a38be1913cae0b68a1ea291cf3
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2018-06-04 11:36:53 +1200

    format script

--- Other/Run-PowerShellAsAdmin.ps1
+++ Other/Run-PowerShellAsAdmin.ps1
@@ -5,18 +5,16 @@
     Purpose: Elevates Powershell as Admin if not already Admin and runs script.
 #>
 
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) 
+{
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) 
+  {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
     
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
     
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-    
-        Exit
-    
-    }
-    
+    Exit
+  }
 }
 
 Install-Module AzureRM
