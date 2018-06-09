1c9d1d8f20263ea56547330eb0a9585333768850
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2018-06-09 22:37:24 +1200

    updated Connect-Azure script

--- Azure/Connect-Azure.ps1
+++ Azure/Connect-Azure.ps1
@@ -1,25 +1,49 @@
-﻿#requires -Version 3.0 -Modules AzureRM
+﻿#requires -Modules AzureRM.Profile
+#requires -version 3.0
+<#
+    .SYNOPSIS
+    Connect-Azure
+
+    .DESCRIPTION
+    Loads the Azure Resource Manager modules, then connects to Azure and opens a Window allowing you to select what subscription.
+
+
+    .NOTES
+    Version:        1.0
+    Author:         Luke Murray (Luke.Geek.NZ)
+    Creation Date:  20/03/17
+    Purpose/Change: 
+    20/03/17 - Intiial script development
+    09/06/2018 - script format cleanup
+
+    .EXAMPLE
+    Connect-Azure
+  
+#>
+
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'Stop'
+
+#Import Modules & Snap-ins
+Import-Module -Name AzureRM -Verbose -Force
+
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
 function Connect-Azure
 {
-  <#
-      .SYNOPSIS
-      Connect-Azure Function to login to Azure - using Resource Groups.
-      .DESCRIPTION
-      Prompts for Azure user credentials, prompts to select Azure Subscription and passes through to select an Azure subscrption.
-      .EXAMPLE
-      Connect-Azure
-      .AUTHOR: Luke Murray (Luke.Geek.NZ)
-      .VERSION: 0.1
-  #>
-  Import-Module -Name AzureRM -Verbose -Force
+  #Prompts for Azure credentials
+  Connect-AzureRmAccount
   
-  #Authenticate to Azure with Azure RM credentials
-  
-  Add-AzureRmAccount
-  
-  #Select Azure Subscription
-  $subscriptionName = (Get-AzureRmSubscription).SubscriptionName | Out-GridView -Title 'Select Azure Subscription' -PassThru
+  #Prompts Window allowing you to select which  Azure Subscription to connect to
+  $subscriptionName = (Get-AzureRmSubscription) | Out-GridView -Title 'Select Azure Subscription' -PassThru
   Set-AzureRmContext -SubscriptionName $subscriptionName
 }
+
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 Connect-Azure
