63cbeb64156227a8c6fe085ac3a3285eefc66504
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2018-06-09 21:24:33 +1200

    Added execution logic, tidied up script formatting.

--- Other/Disable-SMB1.ps1
+++ Other/Disable-SMB1.ps1
@@ -1,11 +1,58 @@
+#requires -Version 3.0 -Modules Dism
+
 <#
-    Author: Luke Murray (Luke.Geek.NZ)
-    Version: 0.1
-    Version History:
-    Purpose: Disables SMB1.
+    .SYNOPSIS
+    Disables SMB1
+
+    .DESCRIPTION
+    This script disables SMB1. If Windows 10 or greater it will use the PowerShell cmdlet to disable SMB1 with no restart, if the OS is
+    less than Windows 10 - such as Windows 7 it will set the services to be disabled manually. Needs to be run as Administrator.
+
+    .PARAMETER <Parameter_Name>
+    None
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    None
+
+    .NOTES
+    Version:        1.0
+    Author:         Luke Murray
+    Creation Date:  14/05/17
+    Purpose/Change: Initial script development
+
+    .EXAMPLE
+    ./Disable-SMB1.ps1
+  
 #>
 
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Stop
+$ErrorActionPreference = 'Stop'
+
+#If script not ran as Administrator, request elevation of privilages and run script.
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) 
+{
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) 
+  {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    
+    Exit
+  }
+}
+
+#Gathers OS version
 $OSVersion = (Get-WmiObject -Class win32_operatingsystem).version
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
 
 try
 {
