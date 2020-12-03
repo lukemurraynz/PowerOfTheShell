1ab5acdf85fe79c2eebcb6e8cb8e8f28341ec245
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2020-12-03 16:54:46 +1300

    Create Windows10.Test.ps1
    Initial commit - Windows 10 pester test.

---
+++ PesterTests/Windows10.Test.ps1
@@ -0,0 +1,11 @@
+﻿Describe 'SMB1 Check' {
+
+  Context 'Check the SMB1 protocol is enabled' {
+
+  It 'Check the SMB1 protocol is enabled' {
+   $a = Get-SmbServerConfiguration | Select EnableSMB1Protocol
+      $a.EnableSMB1Protocol | Should -Be $false
+  }
+
+  }
+  }
