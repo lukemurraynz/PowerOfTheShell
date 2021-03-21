d93336aea142c26215ce4281d32b23762b7802c7
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-03-22 08:38:37 +1300

    Update Export_EventLogs.ps1

--- Other/Export_EventLogs.ps1
+++ Other/Export_EventLogs.ps1
@@ -1,3 +1,18 @@
-﻿# ConfigNew-Item c:\temp -ItemType Directory$logFileName = "Application" # Add Name of the Logfile (System, Application, etc)$path = "C:\temp\" # Add Path, needs to end with a backsplash # do not edit$exportFileName = $logFileName + (get-date -f yyyyMMdd) + ".evt"$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileName}$logFile.backupeventlog($path + $exportFileName)
+﻿# Config
+New-Item c:\temp -ItemType Directory -Force
+$logFileName = "Application" # Add Name of the Logfile (System, Application, etc)
+$path = "C:\temp\" # Add Path, needs to end with a backsplash
+ 
+# do not edit
+$exportFileName = $logFileName + (get-date -f yyyyMMdd) + ".evt"
+$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileName}
+$logFile.backupeventlog($path + $exportFileName)
 
-# Config$logFileName = "System" # Add Name of the Logfile (System, Application, etc)$path = "C:\temp\" # Add Path, needs to end with a backsplash # do not edit$exportFileName = $logFileName + (get-date -f yyyyMMdd) + ".evt"$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileName}$logFile.backupeventlog($path + $exportFileName)
+# Config
+$logFileName = "System" # Add Name of the Logfile (System, Application, etc)
+$path = "C:\temp\" # Add Path, needs to end with a backsplash
+ 
+# do not edit
+$exportFileName = $logFileName + (get-date -f yyyyMMdd) + ".evt"
+$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileName}
+$logFile.backupeventlog($path + $exportFileName)
