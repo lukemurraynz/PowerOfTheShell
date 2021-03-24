ba83cc24c60dafe7e5b2612d9e7eebaa05151119
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-03-25 09:11:41 +1300

    Create Create-LocalUser.ps1
    Initial commit - Create-LocalUser

---
+++ Other/Create-LocalUser.ps1
@@ -0,0 +1,12 @@
+﻿$minLength = 5 ## characters
+$maxLength = 64 ## characters
+$length = Get-Random -Minimum $minLength -Maximum $maxLength
+$nonAlphaChars = 5
+add-type -AssemblyName System.Web
+$password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
+$secPw = ConvertTo-SecureString -String $password -AsPlainText -Force
+
+
+$Username = 'LastNameFirstName'
+New-LocalUser "$Username" -Password $secPw -FullName "FirstName Lastname" -Description "Vendor - Local Administrator Account."
+Add-LocalGroupMember -Group 'Administrators' -Member "$Username" 
