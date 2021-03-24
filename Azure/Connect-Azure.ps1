4c33510e331ec30c759181be8b6f911598f57522
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-03-25 10:05:37 +1300

    Update Connect-Azure.ps1
    Updated for Az.Accounts module.

--- Azure/Connect-Azure.ps1
+++ Azure/Connect-Azure.ps1
@@ -1,4 +1,4 @@
-﻿#requires -Modules AzureRM.Profile
+﻿#requires -Modules Az.Accounts
 #requires -version 3.0
 <#
     .SYNOPSIS
@@ -8,12 +8,13 @@
     Loads the Azure Resource Manager modules, then connects to Azure and opens a Window allowing you to select what subscription.
 
     .NOTES
-    Version:        1.0
+    Version:        1.1
     Author:         Luke Murray (Luke.Geek.NZ)
     Creation Date:  20/03/17
     Purpose/Change: 
     20/03/17 - Intiial script development
     09/06/2018 - script format cleanup
+    25/03/2021 - Updated script (finally) to Az module. Updated to 1.1.
 
     .EXAMPLE
     Connect-Azure
@@ -26,18 +27,18 @@
 $ErrorActionPreference = 'Stop'
 
 #Import Modules & Snap-ins
-Import-Module -Name AzureRM -Verbose -Force
+Import-Module Az.Accounts
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
 function Connect-Azure
 {
   #Prompts for Azure credentials
-  Connect-AzureRmAccount
+  Connect-AzAccount
   
   #Prompts Window allowing you to select which  Azure Subscription to connect to
-  $subscriptionName = (Get-AzureRmSubscription) | Out-GridView -Title 'Select Azure Subscription' -PassThru
-  Set-AzureRmContext -SubscriptionName $subscriptionName
+  $subscriptionName = (Get-AzSubscription) | Out-GridView -Title 'Select Azure Subscription' -PassThru
+  Set-AzContext -SubscriptionName $subscriptionName
 }
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
