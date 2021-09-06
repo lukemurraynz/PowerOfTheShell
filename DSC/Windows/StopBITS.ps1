d92625bf60285bb6b670586022c924f0d4698c27
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-09-06 20:35:31 +1200

    Create StopBITS.ps1

---
+++ DSC/Windows/StopBITS.ps1
@@ -0,0 +1,16 @@
+﻿configuration ExampleDSC
+{
+Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Node localhost
+    {
+        Service StopBITS
+        {
+            Name = 'BITS'
+            Ensure = 'Present'
+            State = 'Stopped'
+        }
+    }
+ 
+}
+ExampleDSC
+Start-DscConfiguration ExampleDSC -Wait -Verbose -Force
