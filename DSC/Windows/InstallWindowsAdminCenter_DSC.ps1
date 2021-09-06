8da785aa54f580c10a673e0d4bb1e009ac89cee2
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-09-06 20:34:30 +1200

    Create InstallWindowsAdminCenter_DSC.ps1

---
+++ DSC/Windows/InstallWindowsAdminCenter_DSC.ps1
@@ -0,0 +1,40 @@
+﻿configuration WindowsAdminCenter
+{
+    param
+    (
+        [System.String]
+        $WacProductId = '{7019BE31-3389-46FB-A077-B813D53C1266}',
+        
+        [System.String]
+        $WacDownloadPath = 'http://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1804.25.msi',
+
+        [System.Int16]
+        $Port = 6516,
+        
+        [System.String]
+        $Thumbprint
+    )
+
+    Import-DscResource -ModuleName PSDscResources
+    
+    if ([System.String]::IsNullOrEmpty($Thumbprint))
+    {
+        $wacInstallArguments = "/qn /l*v c:\windows\temp\windowsadmincenter.msiinstall.log SME_PORT=$Port SSL_CERTIFICATE_OPTION=generate"
+    }
+    else
+    {
+        $wacInstallArguments = "/qn /l*v c:\windows\temp\windowsadmincenter.msiinstall.log SME_PORT=$Port SME_THUMBPRINT=$Thumbprint"
+    }
+    
+    Node localhost
+    {
+        MsiPackage InstallWindowsAdminCenter
+        {
+            ProductId = $WacProductId
+            Path      = $WacDownloadPath
+            Arguments = $wacInstallArguments
+            Ensure    = 'Present'
+        }
+    }
+}
+
