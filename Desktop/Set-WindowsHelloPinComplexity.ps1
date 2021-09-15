7d7d6a079e9fb73111410e055de96e82b749644c
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-09-16 11:06:10 +1200

    Create Set-WindowsHelloPinComplexity.ps1

---
+++ Desktop/Set-WindowsHelloPinComplexity.ps1
@@ -0,0 +1,4 @@
+﻿#Simple script to change Windows Hello PINComplexity to: Minimum of 4 chars, not 6.
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork\PINComplexity' -Force
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork\PINComplexity' -Name 'MinimumPINLength' -Value 4 -PropertyType DWord -Force 
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork\PINComplexity' -Name 'MaximumPINLength' -Value 10 -PropertyType DWord -Force 
