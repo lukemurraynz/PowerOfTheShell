62baa82d8b1d75d232c64059f09cfb114aa0196e
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-11-16 12:06:45 +1300

    Create Get-GroupCreationGroup.ps1

---
+++ Azure/Get-GroupCreationGroup.ps1
@@ -0,0 +1,8 @@
+﻿#Connects to Microsoft Azure
+Connect-AzureAD
+#Displays the values, including the GroupCreationAllowedGroupId:
+(Get-AzureADDirectorySetting).values
+
+# Gets Azure AD group Info
+$id = ''
+Get-AzureADGroup -ObjectId $id| fl
