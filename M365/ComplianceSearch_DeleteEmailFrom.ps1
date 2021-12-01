9f3a498c87addd991f282e0a68f8cd7d158cb2a5
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2021-12-02 08:20:38 +1300

    Create ComplianceSearch_DeleteEmailFrom.ps1

---
+++ M365/ComplianceSearch_DeleteEmailFrom.ps1
@@ -0,0 +1,10 @@
+﻿ #Log into M365
+ Connect-IPPSSession -UserPrincipalName' it@name.co.nz'
+#Start Compliance Search
+ New-ComplianceSearch -Name Phish -ExchangeLocation All -ContentMatchQuery 'from:"badaddress@badcompany.com"'
+ Start-ComplianceSearch -Identity Phish
+ #Get Compliance Search
+ Get-ComplianceSearch -Identity Phish | Format-list *
+ #Delete Emails retrieved from Compliance Search
+ New-ComplianceSearchAction -SearchName Phish -Purge -PurgeType HardDelete
+ Get-ComplianceSearchAction
