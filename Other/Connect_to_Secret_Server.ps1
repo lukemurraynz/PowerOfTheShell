d8af638201bdf787a6f9caea4836ff23b44a91d7
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2022-01-06 09:52:14 +1300

    Update Connect_to_Secret_Server.ps1

--- Other/Connect_to_Secret_Server.ps1
+++ Other/Connect_to_Secret_Server.ps1
@@ -1,6 +1,10 @@
+#Uses the Secret Server Module, to connect to a locally installed Secret Server instance and pulls the credential to be used in scripts.
+# https://www.powershellgallery.com/packages/SecretServer/1.0
+
 Import-Module SecretServer
 Set-SecretServerConfig -Uri https://secretserver.domain.co.nz/secretserver/winauthwebservices/SSWinAuthWebService.asmx
 New-SSConnection #Uses Uri we just set by default
+$secretname = 'MySecret'
 
-$Credential = (Get-Secret -SearchTerm 'username' -IncludeRestricted -as Credential ).Credential
+$Credential = (Get-Secret -SearchTerm $secretname -IncludeRestricted -as Credential ).Credential
 $Credential
